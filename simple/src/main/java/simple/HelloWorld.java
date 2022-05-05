3c1822f68be205367a5d9370ec2b114a975d1a43
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-05 15:10:38 +0530

    hi

--- simple/src/main/java/simple/HelloWorld.java
+++ simple/src/main/java/simple/HelloWorld.java
@@ -1,11 +1,17 @@
 package simple;
-
+import java.io.File;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 public class HelloWorld {
-
+	static Logger logger=Logger.getLogger(HelloWorld.class);
+	
 	public static void main(String[] args) {
-		System.out.print("Hello Eagle");
-		// TODO Auto-generated method stub
-
+		String str="welcome to sonar";
+		
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message...:"+str);
+		logger.error("error message...:"+str);
 	}
-
 }
