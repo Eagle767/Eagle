1393ddb45237022438077692292469b51856c990
Eagle767 <104901002+Eagle767@users.noreply.github.com>
2022-05-05 14:58:20 +0530

    Update HelloWorld.java

--- simple/src/main/java/simple/HelloWorld.java
+++ simple/src/main/java/simple/HelloWorld.java
@@ -3,7 +3,7 @@ package simple;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.print("Hello Eagle");
 		// TODO Auto-generated method stub
 
 	}
