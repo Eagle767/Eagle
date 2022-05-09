7078b58c1095a4a71c8f9a0247d89d6523b1a1b8
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-09 16:23:03 +0530

    hi

--- IfElse/src/ifelse/IforElse.java
+++ IfElse/src/ifelse/IforElse.java
@@ -1,10 +1,23 @@
 package ifelse;
 import java.io.File;
+import java.util.Scanner;
+
 import org.apache.log4j.Logger;
 public class IforElse {
 	static Logger logger=logger.getLogger(IforElse);
 	public static void main(String[] args) {
-		System.out.print(false);		
+		int a,b;
+		Scanner s=new Scanner(System.in);
+		logger.info("Type A Value");
+		a=s.nextInt();
+		logger.info("Type B Value");
+		b=s.nextInt();
+		if(a==b) {
+			System.out.print(true);
+		}
+		else {
+			System.out.print(false);
+		}		
 	}
 
 }
