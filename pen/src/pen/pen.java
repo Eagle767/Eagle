09200c1cd5f6848bb5d4b3ce2f9a4ed6c04523a4
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-11 10:35:45 +0530

    hi

---
+++ pen/src/pen/pen.java
@@ -0,0 +1,14 @@
+package pen;
+
+public class pen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a1=args[2];
+		System.out.print(a1);
+		 System.out.print(args[1]);
+		 System.out.print(args[0]);
+
+	}
+
+}
