cdd7a497dd003a3791dcb6fe5c7cd5fe9e9270ee
Eagle767 <104901002+Eagle767@users.noreply.github.com>
2022-05-18 19:01:13 +0530

    Update pen.java

--- pen/src/pen/pen.java
+++ pen/src/pen/pen.java
@@ -4,7 +4,7 @@ public class pen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a1 =args[2];
+		String a1=args[2];
 		System.out.print(a1);
 		 System.out.print(args[1]);
 		 System.out.print(args[0]);
