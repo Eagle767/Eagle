bea6175c3ff78a2f1952fcdeba772ccf0742adbb
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-18 11:45:08 +0530

    hi

---
+++ Test/src/main/java/pack1/Sample.java
@@ -0,0 +1,22 @@
+package pack1;
+//https://fluvid.com/videos/detail/_LkeEcqMpLU3_QGXk#.YnkBk6okJLI.link
+import pack2.Sample2;//- this will create a namspace link
+/*
+this is block comment
+*/
+//- this is a single line comment
+/**
+this sign of double star is for documentation comment
+*/
+public class Sample{
+	//pack2.Sample2 obj;
+	//pack2.Sample2 obj2;
+	Sample2 obj;
+	Sample2 obj2;
+	public static void main(String args[]){
+System.out.println("Hello Muthu");
+}
+	
+public void water(){
+}
+}
