b3e345d821b80ebdfca8c2bea901c368d5420041
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-24 14:06:19 +0530

    hi

--- Test/src/main/java/Test1/PBV_PBR_Demo.java
+++ Test/src/main/java/Test1/PBV_PBR_Demo.java
@@ -15,12 +15,16 @@ public class PBV_PBR_Demo {
 		
 		logger.info("Size of laddu before PBV..:"+laddu.size);//10
 		new PBV().accept(laddu.size);
+		logger.info("Size of laddu after PBV..:"+laddu.size);
 		logger.info("Size of laddu after PBV..:"+laddu.size);//10
 		
 		
 		logger.info("Size of laddu before PBR..:"+laddu.size);//10
 		new PBR().accept(laddu);
 		logger.info("Size of laddu after PBR...:"+laddu.size);//5
+		
+		Mutability a=new Mutability();
+		a.display();
 	}
 	
 }
@@ -37,3 +41,53 @@ class PBR{
 class Laddu{
 	int size;
 }
+class Mutability {
+	static Logger logger=Logger.getLogger(Mutability.class);
+	void display() {
+		
+		//simple  types - pass by value..
+		int n=100;
+		
+		int k=n;
+		
+		n=n+100;
+		//int k=n;
+		logger.info(n);
+		
+		logger.info(k);
+		
+		String str="hello";
+		
+		String temp=str;
+		
+		str=str+"world............";
+		
+		logger.info(str);
+		logger.info(temp);
+		
+		//complex type - pass by reference
+		World w=new World();
+		World tempw=w;
+		//World tempw=new World();
+		//tempw=w;
+		
+		w.size=110000;
+		
+		logger.info(w.size);
+		logger.info(tempw.size);
+	}
+}
+class World{
+	int size=10;
+}
+
+
+
+
+
+
+
+
+
+
+
