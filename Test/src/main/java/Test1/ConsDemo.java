543e0525c49e24bf571110470d5a64c05da87156
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-24 17:59:50 +0530

    hi

--- Test/src/main/java/Test1/ConsDemo.java
+++ Test/src/main/java/Test1/ConsDemo.java
@@ -1,7 +1,13 @@
 package Test1;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 //https://fluvid.com/videos/detail/p35djuXqDmIaY1VMZ#.YoytmOY-5Qs.link
 public class ConsDemo {
-	
+	static Logger logger=Logger.getLogger(StaticDemo.class);
 	public ConsDemo() {
 		System.out.println("cons with default called...");
 	}
@@ -14,6 +20,8 @@ public class ConsDemo {
 	
 	
 	public static void main(String[] args) {
+		String log4jConfigFile=System.getProperty("user.dir")+File.separator+"log4j.Properties";
+		PropertyConfigurator.configure(log4jConfigFile);
 		new ConsDemo();
 		new ConsDemo(20);
 		new ConsDemo(new Employee());
