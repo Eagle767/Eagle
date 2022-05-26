87de1e16d4f23ee302043bbf946c1f6643ded20b
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-26 17:58:02 +0530

    hi

--- Access/src/Access/ClassTest.java
+++ Access/src/Access/ClassTest.java
@@ -1,41 +1,41 @@
 package Access;
-
 import java.io.File;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 class ClassTest {
-	static Logger logger=Logger.getLogger(ClassTest.class);
+
+	static protected String str="HI It is Protected String";
 	public static void main(String[] args) {
+		Logger logger=Logger.getLogger(ClassTest.class);
 		String log4jConfigFile=System.getProperty("user.dir")+File.separator+"log4j.Properties";
 		PropertyConfigurator.configure(log4jConfigFile);
-		one c=new one();
-		c.abc();
-		int d=c.abc();
-		logger.info(d);
-		Ten a=new Ten();
-		//Twenty b=new Twenty();
 	}
 }
-class one{
-	static Logger logger=Logger.getLogger(ClassTest.class);
-	public one() {
-		logger.warn("It is Public........");
-	}
-	int abc() {
-		return 10;
-	}
-}
-class Ten{
-	static Logger logger=Logger.getLogger(ClassTest.class);
-	protected Ten() {
-		logger.warn("It is Protected.....");
-	}
-}
+
 class Twenty{
-	static Logger logger=Logger.getLogger(ClassTest.class);
-	private Twenty() {
-		logger.warn("It is Private.....");
+	
+	class d{
+		//static hiii h;
+		class s{
+			void meth() {
+				int k=5;
+			}
+			//static class hiii{}
+		}
+		
+		//static class hiii{}
+			
+		
+	}
+	static class ABC{
+	    class hi {
+	    	// static class b{}
+	 	    	
+	 	    
+		}
+	    static class b{
+	    	
+	    }
 	}
 }
