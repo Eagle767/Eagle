e72efb6986278ef4f6299f4c486f1ae24c4c2603
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-05-30 18:11:29 +0530

    hi

---
+++ Test/src/main/java/Test1/InheDemo.java
@@ -0,0 +1,46 @@
+package Test1;
+//https://fluvid.com/videos/detail/n8Qdni_r2yIZnOEB_#.YpS6iTDeh8c.link
+public class InheDemo {
+	public static void main(String[] args) {
+		Dominos dominos=new JahnaviPizza();
+		dominos.sellPizza();
+	}
+}
+abstract class Dominos{
+	private void makeDough() {
+		System.out.println("dough made as per the dominos logic..");
+	}
+	private void bakePizza() {
+		System.out.println("pizza baked as the dominos logic..");
+	}
+	private void addToppings() {
+		System.out.println("toppings added as per the dominos logic..");
+	}
+	private void packing() {
+		System.out.println("packing done as per dominos logic...");
+	}
+	final public void makePizza() {//this method is called template method
+		makeDough();
+		bakePizza();
+		addToppings();
+		packing();
+	}
+	final public void sellPizza() {//this method is called template method
+		collectMoney();
+		makePizza();
+		delivery();
+	}
+	public abstract void delivery();
+	public abstract void collectMoney();
+}
+class JahnaviPizza extends Dominos{
+	@Override
+	public void collectMoney() {
+		System.out.println("money is collected in US Dollors..");
+	}
+	@Override
+	public void delivery() {
+		System.out.println("delivery done via benz car....");
+	}
+	
+}
