51295e8b63a1625f939b536f8e3be151c042e9e5
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-06-08 11:07:03 +0530

    hi

---
+++ LabWork/src/labWork1/Multiply2Numbers.java
@@ -0,0 +1,22 @@
+package labWork1;
+import java.io.File;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Multiply2Numbers {
+	static Logger logger=Logger.getLogger( Multiply2Numbers.class);
+	public static void main(String[] args) {
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		// TODO Auto-generated method stub
+		logger.info("Enter two numbers");
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		logger.info("The Given Numbers Are "+a+", "+b);
+		int mul=a*b;
+		logger.info("The Multiplication of Two Numbers are "+mul);
+	}
+}
