16df6347b83c53855b7e066006ca38c349c77f9d
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-06-20 17:45:20 +0530

    hi

---
+++ LoggerTest/src/LoggerTest/LoggTest.java
@@ -0,0 +1,25 @@
+package LoggerTest;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+
+public class LoggTest {
+
+	static Logger logger=Logger.getLogger(LoggTest.class);
+	
+	public static void main(String[] args) {
+		
+		String a=System.getProperty("user.dir")+File.separator+"pro";
+		
+		PropertyConfigurator.configure(a);
+		
+		logger.info(a);
+		
+		logger.info(System.getProperty("user.dir"));
+		
+	}
+	
+}
