c1ec904a0ec900cc669090f7bf5d62f3a79ba1fe
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-06-24 14:43:32 +0530

    hi

--- TravelsProject/src/travelProjectFirst/SwitchCase.java
+++ TravelsProject/src/travelProjectFirst/SwitchCase.java
@@ -2,25 +2,35 @@ package travelProjectFirst;
 
 import java.time.DayOfWeek;
 
+import java.time.LocalDateTime;
+
 public class SwitchCase {
 	
 	public static void main(String[] args) {
 		
-		int t=7;
+		double t=4;
 		
+		LocalDateTime l1=LocalDateTime.now();
 		
-		
-		for(int i=1;i<=t;i++) {
+		for(int i=0;i<=t;i++) {
 			
-			DayOfWeek Day=DayOfWeek.of(i);
+			DayOfWeek Day=l1.getDayOfWeek().plus(i);
 			
 			switch (Day) {
 			case SUNDAY:{
 				System.out.println("Sunday......");
+				t++;
 				break;
 			}
 			case SATURDAY:{
+				if(((l1.getMonthValue()==1)&&(l1.getDayOfMonth()==1))
+						||((l1.getMonthValue()==1)&&(l1.getDayOfMonth()==26))
+						||((l1.getMonthValue()==8)&&(l1.getDayOfMonth()==15))){
+					t++;
+				}
+				else {
 				System.out.println("SaturDay..........");
+				t=t+0.5;}
 				break;
 			}	
 
@@ -31,6 +41,9 @@ public class SwitchCase {
 			}
 			
 		}
+		System.out.println((t%1)*30);
+		
+		System.out.println(l1.toLocalTime());
 		
 	}
 
