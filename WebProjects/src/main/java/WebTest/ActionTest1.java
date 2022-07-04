0811f00f26549d374b37ff67d0c7e5b2c7f4dc4b
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-07-04 17:32:31 +0530

    hi

---
+++ WebProjects/src/main/java/WebTest/ActionTest1.java
@@ -0,0 +1,25 @@
+package WebTest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Action")
+public class ActionTest1 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doPost(request, response);
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println("Service method is called............");
+		
+	}
+
+}
