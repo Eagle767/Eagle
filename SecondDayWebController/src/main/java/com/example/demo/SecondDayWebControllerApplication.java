67fb36a22851f6b78eaced09d7239c0d2bf22f22
muthukumar.m <muthukumar.m@DT150157.QICHOAPPL.com>
2022-07-28 14:38:53 +0530

    How to find old session id attributes use(jsessionid=oldsessionid)

--- SecondDayWebController/src/main/java/com/example/demo/SecondDayWebControllerApplication.java
+++ SecondDayWebController/src/main/java/com/example/demo/SecondDayWebControllerApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-@ComponentScan(basePackages = {"controlpack"})
+@ComponentScan(basePackages = {"controlpack","sessionpack"})
 @SpringBootApplication
 public class SecondDayWebControllerApplication {
 
--- SecondDayWebController/src/main/webapp/hello.jsp
+++ SecondDayWebController/src/main/webapp/hello.jsp
@@ -1,1 +1,2 @@
 <h1>Hello JSP</h1>
+
